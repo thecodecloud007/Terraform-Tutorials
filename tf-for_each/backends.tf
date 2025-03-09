949050f1326a5cc744642e0eaf0e53a93e81226b
Ravindra Kumar <thecodecloud.in@gmail.com>
2025-03-09 13:34:17 +0530

    removed secret

--- tf-for_each/backends.tf
+++ tf-for_each/backends.tf
@@ -2,6 +2,6 @@ terraform {
     backend "gcs" {
         bucket = "my-gcp-project-x-state"
         prefix = "terraform/state"
-        credentials = "gcs-secret.json"
+        credentials = "x"
     }
 }
